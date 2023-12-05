912f800efbb6404e89d9eae8081c1f68d2ffebd9
Francesco Losciale <francesco.losciale@mypulse.ai>
2023-12-05 15:43:53 +0000

    improvements

--- src/liberator_tutorial/todos_resource.clj
+++ src/liberator_tutorial/todos_resource.clj
@@ -2,12 +2,23 @@
   (:require [liberator.core :as liberator]
             [jason.convenience :as json]))
 
+(defn ->todo-list
+  [request]
+  (-> request
+      :body
+      (json/<-wire-json)))
+
+(defn create-todo-list
+  [todo-list]
+  (assoc todo-list :id 1))
+
 (def resource
   (liberator/resource
     :allowed-methods [:get :post]
     :available-media-types ["application/json"]
-    :post! (fn [context])
+    :post! (fn [{:keys [request]}]
+             {:todo-list (create-todo-list (->todo-list request))})
     :handle-ok (fn [{:keys [request]}]
                  (json/->wire-json []))
-    :handle-created (fn [context]
-                      (json/->wire-json []))))
+    :handle-created (fn [{:keys [todo-list]}]
+                      (json/->wire-json todo-list))))
--- test/liberator_tutorial/todos_test.clj
+++ test/liberator_tutorial/todos_test.clj
@@ -3,18 +3,20 @@
     [clojure.test :refer :all]
     [jason.convenience :as json]
     [liberator-tutorial.shared.fixtures :refer [running-server server-base-url]]
+    [liberator-tutorial.todos-resource :as todos-resource]
     [org.httpkit.client :as http]))
 
 (use-fixtures :once running-server)
 
-(deftest can-get-todos
+(deftest can-get-todo-lists
   (let [response @(http/get (str server-base-url "/todos") {:as :text})]
     (testing "returns 200"
       (is (= 200 (:status response)))
-      (is (= [] (json/<-wire-json (:body response)))))))
+      (is (= [] (todos-resource/->todo-list response))))))
 
-(deftest can-post-todo
-  (let [response @(http/post (str server-base-url "/todos") {:body (json/->wire-json [])})]
+(deftest can-post-todo-list
+  (let [todo-list {:todos []}
+        response @(http/post (str server-base-url "/todos") {:body (json/->wire-json todo-list)})]
     (testing "returns 201"
       (is (= 201 (:status response)))
-      (is (= [] (json/<-wire-json (:body response)))))))
+      (is (= {:id 1 :todos []} (todos-resource/->todo-list response))))))
